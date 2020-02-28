5629eb2eecffb322e3072084f6f8e30c760b1e92
Annika Chauhan <annikachauhan@Annikas-MacBook-Pro.local>
2020-02-28 08:20:28 +0000

    my brand favicon added for continuity

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import styles from "./Header.module.scss";
+
+class Header extends Component {
+    render(){
+        return(
+            <h1>Travel</h1>
+        );
+    }
+}
+
+export default Header;
